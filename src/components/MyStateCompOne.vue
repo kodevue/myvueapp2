24764a50d492f0fe32e37399acb34b47f8a62125
santoshkumarm1 <santoshkumarm1@verifone.com>
2023-07-25 20:02:34 +0530

    state mangment

--- src/components/MyStateCompOne.vue
+++ src/components/MyStateCompOne.vue
@@ -1,5 +1,14 @@
-<script setup></script>
+<script setup>
+import { useCounterStore } from '@/stores/counter'
+
+let counterStore = useCounterStore()
+</script>
 
 <template>
   <h1>Componente 1</h1>
+  <h2>{{ counterStore.count }} / {{ counterStore.doubleCount }}</h2>
+
+  <button @click="counterStore.count++">Click Me</button>
+  <button @click="counterStore.increment">Click Me1</button>
+  <button @click="counterStore.increment()">Click Me2</button>
 </template>
--- src/components/MyStateCompTwo.vue
+++ src/components/MyStateCompTwo.vue
@@ -1,5 +1,17 @@
-<script setup></script>
+<script setup>
+import { storeToRefs } from 'pinia'
+import { useCounterStore } from '@/stores/counter'
+
+let counterStore = useCounterStore()
+let { count, doubleCount } = storeToRefs(counterStore)
+
+let { increment } = counterStore
+</script>
 
 <template>
   <h1>Componente 2</h1>
+  <h1>{{ count }} / {{ doubleCount }}</h1>
+  <input type="button" value="Click Me" @click="count++" />
+  <input type="button" value="Click Me1" @click="increment" />
+  <input type="button" value="Click Me1" @click="increment()" />
 </template>
