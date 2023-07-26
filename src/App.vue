6b086174f31c9a1ffb16e87dbacaa70d7e701148
santoshkumarm1 <santoshkumarm1@verifone.com>
2023-07-26 13:49:20 +0530

    displaying component using component.

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,17 @@
 <script setup>
+import { computed, ref } from 'vue'
 import MyNavbar from './components/MyNavbar.vue'
 import MyStateCompOne from './components/MyStateCompOne.vue'
 import MyStateCompTwo from './components/MyStateCompTwo.vue'
+
+const active = ref(true)
+const currentView = computed(() => (active.value ? MyStateCompOne : MyStateCompTwo))
 </script>
 
 <template>
   <MyNavbar />
-  <MyStateCompOne />
-  <MyStateCompTwo />
+
+  <input type="button" value="Toggle" @click="active = !active" />
+  <component :is="currentView"></component>
 </template>
 
